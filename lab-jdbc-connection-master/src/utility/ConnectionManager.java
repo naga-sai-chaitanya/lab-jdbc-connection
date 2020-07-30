6a6b67130c39ac83470e6138bb4ee50522a1cac6
Naga Sai Chaitanya <55658505+naga-sai-chaitanya@users.noreply.github.com>
2020-07-30 17:21:19 +0530

    Update ConnectionManager.java

--- lab-jdbc-connection-master/src/utility/ConnectionManager.java
+++ lab-jdbc-connection-master/src/utility/ConnectionManager.java
@@ -1,9 +1,29 @@
 package utility;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionManager {
+	
+	public static Connection getConnection() throws Exception
+	{
+		Properties prop = loadPropertiesFile();
+		
+		String driver = prop.getProperty("driver");
+		String url = prop.getProperty("url");
+		String user =prop.getProperty("username");
+		String pass = prop.getProperty("password");
+		
+		Class.forName(driver);
+		Connection con = DriverManager.getConnection(url,user,pass);
+		
+		return con;
+		
+	}
 	public static Properties loadPropertiesFile() throws Exception{
 		Properties prop = new Properties();
 		InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
@@ -14,3 +34,4 @@ public class ConnectionManager {
 	}
 	
 }
+
